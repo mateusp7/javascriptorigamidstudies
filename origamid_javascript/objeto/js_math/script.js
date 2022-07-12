fee6a7f0a8414ebdf75fb933208907ca92159ecc
Mateusdev7 <mateusdev1337@gmail.com>
2022-07-12 16:32:28 -0300

    Conteudo da biblioteca Math

---
+++ origamid_javascript/objeto/js_math/script.js
@@ -0,0 +1,15 @@
+console.log(Math.PI)
+
+console.log(Math.abs(3 - 5)); // Retorna o módulo do valor
+console.log(Math.ceil(4.8334)); // Sempre arredonda pra cima
+console.log(Math.floor(4.833)); // Sempre arredonda pra baixo
+console.log(Math.floor(4.3)); // Sempre arredonda pra baixo
+console.log(Math.round(4.3)); // Arredonda da forma correta
+console.log(Math.round(4.5)); // Arredonda da forma correta
+
+console.log(Math.max(5,3,10,12,50,1)) // Retorna o maior valor
+console.log(Math.min(5,3,10,12,50,1)) // Retorna o menor valor
+
+console.log(Math.floor(Math.random() * 100)) // Retorna o maior valor
+console.log(Math.floor(Math.random() * (40 - 20 + 1) + 20)) // Retorna o maior valor
+
