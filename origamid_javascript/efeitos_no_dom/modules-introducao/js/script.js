71389901651c817cffff9a4c225e8068d4a2f804
Mateusdev7 <mateusdev1337@gmail.com>
2022-07-23 09:45:45 -0300

    Aula sobre Modules

---
+++ origamid_javascript/efeitos_no_dom/modules-introducao/js/script.js
@@ -0,0 +1,8 @@
+import initScrollSuave from './modules/scroll-suave.js';
+initScrollSuave();
+import initAnimacaoScroll from './modules/animacao-scroll.js';
+initAnimacaoScroll();
+import initTabNav from './modules/animacao-tab.js';
+initTabNav();
+import initAccordion from './modules/animacao-accordion.js';
+initAccordion();
