71b3cf28ada8cf06d55c1f77f5dfeb6ba7de1e2e
Mateusdev7 <mateusdev1337@gmail.com>
2022-07-24 12:20:29 -0300

    Aula sobre Modal e aplicação no site

---
+++ origamid_javascript/efeitos_no_dom/modal/js/script.js
@@ -0,0 +1,10 @@
+import initScrollSuave from './modules/scroll-suave.js';
+initScrollSuave();
+import initAnimacaoScroll from './modules/animacao-scroll.js';
+initAnimacaoScroll();
+import initTabNav from './modules/animacao-tab.js';
+initTabNav();
+import initAccordion from './modules/animacao-accordion.js';
+initAccordion();
+import initModal from './modules/modal.js';
+initModal();
