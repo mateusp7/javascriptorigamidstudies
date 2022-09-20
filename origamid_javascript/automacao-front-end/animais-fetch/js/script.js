88e4409d2c4d76cbd586476a4da5b77d0ed3b66a
Mateusdev7 <mateusdev1337@gmail.com>
2022-09-20 14:56:18 -0300

    ESLint Animais Fetch

---
+++ origamid_javascript/automacao-front-end/animais-fetch/js/script.js
@@ -0,0 +1,21 @@
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+import initTabNav from './modules/animacao-tab.js';
+import initAccordion from './modules/animacao-accordion.js';
+import initModal from './modules/modal.js';
+import initToolTip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu-mobile.js';
+import initFuncionamento from './modules/funcionamento.js';
+import initFetchAnimais from './modules/fetch-animais.js';
+
+initScrollSuave();
+initAnimacaoScroll();
+initTabNav();
+initAccordion();
+initModal();
+initToolTip();
+initDropdownMenu();
+initMenuMobile();
+initFuncionamento();
+initFetchAnimais();
